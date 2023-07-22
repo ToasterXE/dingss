4b61099016c9439553202239ef31f593a7f1e5d3
ToasterXE <114070865+ToasterXE@users.noreply.github.com>
2023-07-22 16:44:48 +0200

    e

---
+++ hashseed.py
@@ -0,0 +1,12 @@
+import hashlib
+seed = "12"
+
+def update_seed(string):
+    global seed
+    seed += string
+
+def get_hash(seed):
+    return int.from_bytes(hashlib.sha256(seed.encode('utf-8')).digest(), 'big')
+
+def get_seedval():
+    return(int(str(abs(get_hash(seed)))))
